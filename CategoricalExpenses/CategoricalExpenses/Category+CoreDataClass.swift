77693686f92939d36e028ec443bb884833a81168
Tianyuan Deng <wergelesn@missouri.edu>
2018-11-02 18:10:39 -0500

    Update CategoricalExpense

---
+++ CategoricalExpenses/CategoricalExpenses/Category+CoreDataClass.swift
@@ -0,0 +1,30 @@
+//
+//  Category+CoreDataClass.swift
+//  CategoricalExpenses
+//
+//  Created by Deng tianyuan on 11/2/18.
+//  Copyright © 2018 Shawn Moore. All rights reserved.
+//
+//
+
+import UIKit
+import CoreData
+
+@objc(Category)
+public class Category: NSManagedObject {
+    var expenses: [Expense]? {
+        return self.rawExpenses?.array as? [Expense]
+    }
+    
+    convenience init?(title: String) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        guard let context = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        
+        self.init(entity: Category.entity(), insertInto: context)
+        
+        self.title = title
+    }
+}
